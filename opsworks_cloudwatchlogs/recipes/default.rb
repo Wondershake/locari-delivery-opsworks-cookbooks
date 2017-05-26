530876fc4d8fd4da9374e78e38978cc750be26e8
Greg <greg@toki.waseda.jp>
2017-05-26 22:25:33 +0900

    fuck it

--- opsworks_cloudwatchlogs/recipes/default.rb
+++ opsworks_cloudwatchlogs/recipes/default.rb
@@ -1,5 +1,5 @@
-if node["opsworks"]["cloud_watch_logs_configurations"].any?
-  include_recipe "opsworks_cloudwatchlogs::install"
-else
-  include_recipe "opsworks_cloudwatchlogs::uninstall"
-end
+# if node["opsworks"]["cloud_watch_logs_configurations"].any?
+#   include_recipe "opsworks_cloudwatchlogs::install"
+# else
+#   include_recipe "opsworks_cloudwatchlogs::uninstall"
+# end
